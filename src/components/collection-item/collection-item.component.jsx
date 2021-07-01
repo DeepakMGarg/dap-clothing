ebd1c7eff738b0f644f86eec179db8d8662ab549
DeepakMGarg <deepakgargdg700@gmail.com>
2021-07-02 03:19:20 +0530

    added addItem and removeItem functionality to checkout item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
 
 import './collection-item.styles.scss';
-import { addItem } from '../../redux/cart/cart.actions';
 
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
-  return(
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-    </div>
-    <CustomButton onClick={() =>addItem(item)} inverted >
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
       ADD TO CART
-    </CustomButton>
-  </div>
-)};
+      </CustomButton>
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
